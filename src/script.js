9909109ec5954437238b07957dc8a21dd6c7cc41
Ethan Hsu <ethanhsu128@gmail.com>
2022-05-20 19:12:04 -0700

    ...

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// chrome.runtime.onMessage.addListener(() => {
+//   const optionsEl = document.querySelector(`[aria-label='More options']`);
+//   optionsEl.click();
+//   console.log("working");
+// });
+// const videoEl = document.querySelector(`[aria-label='Video']`);
+
+const optionsEl = document.querySelector(`[aria-label='More options']`);
