bd0ac594aed085c5c3d318ececab12d6cdaf8fa9
Ethan Hsu <ethanhsu128@gmail.com>
2022-05-21 20:56:30 -0700

    asdfjl;

--- react/src/pages/Content/index.js
+++ react/src/pages/Content/index.js
@@ -1,7 +1,7 @@
 import html2canvas from 'html2canvas';
 
 const everything = () => {
-  const video = document.querySelectorAll(`Gv1mTb-aTv5jf`)[1];
+  const video = document.querySelector(`.p2hjYe`);
   html2canvas(video, {
     allowTaint: true,
     useCORS: true,
